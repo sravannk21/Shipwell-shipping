dc1ce019ef02508576de865494b1e1697f751477
Sravan NK <hiraone@proton.me>
2023-06-28 16:57:04 +0530

    extra text removed in what we offer in index page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 $('document').ready(function() {
     $(window).on('resize', function() {
-      console.log('teting')
         if ($(window).width() <= 768) {
           $('.about-box').css('background-image', 'none',);
           $('.js-why-choose').css('color', '#000',);
         }
-      });   
+      });
 })
